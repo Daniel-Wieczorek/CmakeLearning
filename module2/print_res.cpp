6ae3c47b47182521de7e7f8c22985e97041ec0c3
DanielWieczorek <danielwieczorek96@gmail.com>
2022-07-28 21:25:16 +0200

    dev: Cmake course

---
+++ module2/print_res.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "print_res.h"
+
+void print_result(std::string result_type, float result_value)
+{
+    std::cout << result_type << " result: " << result_value << std::endl;
+}
